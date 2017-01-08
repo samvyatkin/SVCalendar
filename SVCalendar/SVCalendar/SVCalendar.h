c01dd5de124f55150741edc184abb872b3863cca
Semypn Vyatkin <samvyto@gmail.com>
2017-01-08 17:39:13 +0300

    Updated files structure

--- SVCalendar/SVCalendar/SVCalendar.h
+++ SVCalendar/SVCalendar/SVCalendar.h
@@ -16,4 +16,3 @@ FOUNDATION_EXPORT const unsigned char SVCalendarVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SVCalendar/PublicHeader.h>
 
-
